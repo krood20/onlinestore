3c3e10bf9fe3c5e408aeef669a13f798be624579
krood20 <krood20@gwmail.gwu.edu>
2018-02-21 12:39:07 -0500

    tried to get database working

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-  $servername = "localhost";
+  $servername = "localhost:8000";
   $username = "root";
-  $password = "Soccer22";
-  $dbname = "information_schema";
+  $password = "";
+  $dbname = "krood20";
 
-$connect = mysqli_connect($servername, $username, $password, $username);
+$connect = mysqli_connect("localhost:8000", "root", "", "krood20");
 
 //checks if you added to the cart
 if(isset($_POST["add_to_cart"]))
@@ -75,8 +75,17 @@ if(isset($_GET["action"]))
          <div class="container" style="width:700px;">
               <h3 align="center">Simple PHP Mysql Shopping Cart</h3><br />
               <?php
-              $query = "SELECT * FROM tbl_product ORDER BY id ASC";
+              $query = "SELECT * FROM products, price ORDER BY id ASC";
               $result = mysqli_query($connect, $query);
+
+              //error handling for a failed connection
+              if (mysqli_connect_errno()){
+                  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                  //you need to exit the script, if there is an error
+                  exit();
+              }
+
+
               if(mysqli_num_rows($result) > 0)
               {
                    while($row = mysqli_fetch_array($result))
@@ -87,10 +96,10 @@ if(isset($_GET["action"]))
                         <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
                              <img src="<?php echo $row["image"]; ?>" class="img-responsive" /><br />
                              <h4 class="text-info"><?php echo $row["name"]; ?></h4>
-                             <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
+                             <h4 class="text-danger">$ <?php echo $row["price.price"]; ?></h4>
                              <input type="text" name="quantity" class="form-control" value="1" />
                              <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
-                             <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+                             <input type="hidden" name="hidden_price" value="<?php echo $row["price.price"]; ?>" />
                              <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
                         </div>
                    </form>
--- products.sql
+++ products.sql
@@ -1,17 +1,28 @@
 --
 -- Table structure for table `tbl_product`
 --
-CREATE TABLE IF NOT EXISTS `tbl_product` (
+CREATE TABLE IF NOT EXISTS `products` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 `image` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+CREATE TABLE IF NOT EXISTS `price` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
 `price` double(10,2) NOT NULL,
 PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;  
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
 --
 -- Dumping data for table `tbl_product`
 --
-INSERT INTO `tbl_product` (`id`, `name`, `image`, `price`) VALUES
-(1, 'Samsung J2 Pro', '1.jpg', 100.00),
-(2, 'HP Notebook', '2.jpg', 299.00),
-(3, 'Panasonic T44 Lite', '3.jpg', 125.00);
+INSERT INTO `products` (`id`, `name`, `image`) VALUES
+(1, 'Samsung J2 Pro', 'broccoli.jpg'),
+(2, 'HP Notebook', 'carrot.jpg'),
+(3, 'Panasonic T44 Lite', 'potato.jpg');
+
+INSERT INTO `price` (`id`, `price`) VALUES
+(1, 5),
+(2, 10),
+(3, 15);
