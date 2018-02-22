70e684f641badd7b9f205cd4310cc725e97e678c
krood20 <krood20@gwmail.gwu.edu>
2018-02-22 17:40:53 -0500

    changed so much stuff

--- index.php
+++ index.php
@@ -63,20 +63,24 @@ if(isset($_GET["action"]))
          }
     }
 }
+//html and php
 ?>
 <!DOCTYPE html>
 <html>
     <head>
          <title>Kyle's Place</title>
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
     <body>
          <br />
          <div class="container" style="width:700px;">
               <h3 align="center">Welcome to Kyle's Place</h3><br />
                 <h4 align="center">The place for memes, groceries, and all your tech needs!</h4><br/>
+
+                <form action="" method="post">
+                  <input type="text" name="search">
+                  <input type="submit" name="submit" value="Search">
+                </form>
+
               <?php
               $query = "SELECT * FROM products ORDER BY id ASC";
               $query1 = "SELECT * FROM price ORDER BY id ASC";
@@ -90,30 +94,39 @@ if(isset($_GET["action"]))
                   exit();
               }
 
+              //search the database --> one query for the categories and one for the names
+              $search_value=$_POST["search"];
+              $sql="SELECT * FROM products WHERE category LIKE '%$search_value%'";
+              $sql1="SELECT * FROM products WHERE name LIKE '%$search_value%'";
+              $result3 = mysqli_query($connect, $sql);
+              $result4 = mysqli_query($connect, $sql1);
 
-              if(mysqli_num_rows($result) > 0 and mysqli_num_rows($result1) > 0)
-              {
-                   while($row = mysqli_fetch_array($result) and $row1 = mysqli_fetch_array($result1))
-                   {
-              ?>
-              <div class="col-md-4">
-                   <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
-                        <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
-                             <h4 class="text-info"><?php echo $row["name"]; ?></h4>
-                             <h4 class="text-danger">$ <?php echo $row1["price"]; ?></h4>
-                             <h4 class="text-danger">Category: <?php echo $row["category"]; ?></h4>
-                             <h4 class="text-danger">Stock: <?php echo $row["stock"]; ?></h4>
-                             <input type="text" name="quantity" class="form-control" value="1" />
-                             <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
-                             <input type="hidden" name="hidden_category" value="<?php echo $row["category"]; ?>" />
-                             <input type="hidden" name="hidden_price" value="<?php echo $row1["price"]; ?>" />
-                             <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
-                        </div>
-                   </form>
-              </div>
-              <?php
-                   }
-              }
+                //general store page --> check if this if statement is right
+                if(mysqli_num_rows($result) > 0 and mysqli_num_rows($result1) > 0)
+                {
+                     while($row = mysqli_fetch_array($result) and $row1 = mysqli_fetch_array($result1))
+                     {
+                ?>
+
+
+                <div class="col-md-4">
+                     <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
+                          <div style="border:1px solid #333; border-radius:5px; padding:16px;" align="center">
+                               <h4 class="text-info"><?php echo $row["name"]; ?></h4>
+                               <h4 class="text-danger">$ <?php echo $row1["price"]; ?></h4>
+                               <h4 class="text-danger">Category: <?php echo $row["category"]; ?></h4>
+                               <h4 class="text-danger">Stock: <?php echo $row["stock"]; ?></h4>
+                               <input type="text" name="quantity" class="form-control" value="1" />
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                               <input type="hidden" name="hidden_category" value="<?php echo $row["category"]; ?>" />
+                               <input type="hidden" name="hidden_price" value="<?php echo $row1["price"]; ?>" />
+                               <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                          </div>
+                     </form>
+                </div>
+                <?php
+                     }
+                }
               ?>
               <div style="clear:both"></div>
               <br />
@@ -121,11 +134,11 @@ if(isset($_GET["action"]))
               <div class="table-responsive">
                    <table class="table table-bordered">
                         <tr>
-                             <th width="40%">Item Name</th>
-                             <th width="10%">Quantity</th>
+                             <th width="20%">Item Name</th>
+                             <th width="20%">Quantity</th>
                              <th width="20%">Price</th>
                              <th width="15%">Total</th>
-                             <th width="5%">Action</th>
+                             <th width="20%">Action</th>
                         </tr>
                         <?php
                         if(!empty($_SESSION["shopping_cart"]))
@@ -136,12 +149,15 @@ if(isset($_GET["action"]))
                         ?>
                         <tr>
                              <td><?php echo $values["item_name"]; ?></td>
-                             <td><?php echo $values["item_quantity"]; ?></td>
+                             <td>-------><?php echo $values["item_quantity"]; ?></td>
                              <td>$ <?php echo $values["item_price"]; ?></td>
                              <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
                              <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
                         </tr>
                         <?php
+                                  //update the stock
+                                  //$row["stock"] = "UPDATE stock SET products.stock = products.stock - '$values["item_quantity"]'";
+
                                   $total = $total + ($values["item_quantity"] * $values["item_price"]);
                              }
                         ?>
--- products.sql
+++ products.sql
@@ -64,3 +64,9 @@ INSERT INTO `price` (`id`, `price`) VALUES
 (19, 200),
 (20, 20),
 (21, 100);
+
+
+/*
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
